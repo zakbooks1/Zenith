eed94bcf11cdfa0aea27dc58e8c6d12f4d2ca4c3
zakbooks <zak.books1@gmail.com>
2026-04-18 19:03:38 -0500

    Fix: Switch to ToadsWorld Bare server

--- static/assets/mathematics/config.js
+++ static/assets/mathematics/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/a/",
-  bare: "https://bare.tomp.app/",
+  bare: "https://bare.toadsworld.org/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/assets/mathematics/handler.js?v=9-30-2024",
