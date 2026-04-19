e90c1c56009e0b07b1eb78c2aa259d05bb4a7d6d
zakbooks1 <zak.books1@gmail.com>
2026-04-18 19:05:51 -0500

    Update config.js

--- static/assets/mathematics/config.js
+++ static/assets/mathematics/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/a/",
-  bare: "https://bare.toadsworld.org/",
+  bare: "https://bare.light-speed.tech/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/assets/mathematics/handler.js?v=9-30-2024",
