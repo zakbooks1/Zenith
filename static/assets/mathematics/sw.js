aa58c7af38262610d0f4bffa3469bcd3ee8b5dac
zakbooks <zak.books1@gmail.com>
2026-04-18 20:38:17 -0500

    Infrastructure: Deploy Nuclear Edition of sw.js for maximum unblocking

--- static/assets/mathematics/sw.js
+++ static/assets/mathematics/sw.js
@@ -1,4 +1,4 @@
-/* Zenith Core Service Worker - Google Optimizer Edition (ES6+) */
+/* Zenith Core Service Worker - Nuclear Edition (ES6+) */
 importScripts("/assets/mathematics/bundle.js?v=9-30-2024");
 importScripts("/assets/mathematics/config.js?v=9-30-2024");
 
@@ -12,6 +12,8 @@ class UVServiceWorker extends EventEmitter {
       typeof e.bare === "string"
         ? [new URL(e.bare, location)]
         : e.bare.map((item) => new URL(item, location));
+    
+    // We strip everything that could possibly block or detect the proxy
     this.headers = {
       csp: [
         "cross-origin-embedder-policy",
@@ -31,7 +33,10 @@ class UVServiceWorker extends EventEmitter {
         "x-powered-by",
         "x-xss-protection",
         "report-to",
-        "nel"
+        "nel",
+        "trusted-types",
+        "require-trusted-types-for",
+        "cross-origin-resource-policy"
       ],
       forward: ["accept-encoding", "connection", "content-length"],
     };
@@ -41,10 +46,6 @@ class UVServiceWorker extends EventEmitter {
     this.browser = Ultraviolet.Bowser.getParser(
       self.navigator.userAgent,
     ).getBrowserName();
-    if (this.browser === "Firefox") {
-      this.headers.forward.push("user-agent");
-      this.headers.forward.push("content-type");
-    }
   }
 
   async fetch({ request: e }) {
@@ -74,18 +75,11 @@ class UVServiceWorker extends EventEmitter {
       
       if (e.referrer?.startsWith(location.origin)) {
         const refUrl = new URL(t.sourceUrl(e.referrer));
-        if (n.headers.origin || (t.meta.url.origin !== refUrl.origin && e.mode === "cors")) {
-          n.headers.origin = refUrl.origin;
-        }
         n.headers.referer = refUrl.href;
       }
       
       const s = (await t.cookie.getCookies(r)) || [];
       const i = t.cookie.serialize(s, t.meta, false);
-      
-      if (this.browser === "Firefox" && e.destination !== "iframe" && e.destination !== "document") {
-        n.forward.shift();
-      }
       
       if (i) {
         n.headers.cookie = i;
@@ -99,43 +93,25 @@ class UVServiceWorker extends EventEmitter {
       }
       
       const a = await fetch(n.send);
-      if (a.status === 500) {
-        return Promise.reject(new Error("Internal Server Error from Engine"));
-      }
-      
       const c = new ResponseContext(n, a, this);
-      const u = new HookEvent(c, null, null);
-      this.emit("beforemod", u);
-      if (u.intercepted) {
-        return u.returnValue;
+      
+      // NUCLEAR STRIP: Remove every single security header
+      for (const header of this.headers.csp) {
+        delete c.headers[header];
       }
       
-      // DEEP STRIP: Remove all security headers that break Google
-      for (const header of this.headers.csp) {
-        if (c.headers[header]) {
-          delete c.headers[header];
+      // Also delete any header starting with 'x-content-' or 'x-frame-'
+      for (const key in c.headers) {
+        if (key.startsWith('x-content-') || key.startsWith('x-frame-') || key.startsWith('content-security-')) {
+          delete c.headers[key];
         }
       }
-      
+
       if (c.headers.location) {
         c.headers.location = t.rewriteUrl(c.headers.location);
       }
       
-      if (c.headers["set-cookie"]) {
-        Promise.resolve(
-          t.cookie.setCookies(c.headers["set-cookie"], r, t.meta),
-        ).then(() => {
-          self.clients.matchAll().then((clients) => {
-            clients.forEach((client) => {
-              client.postMessage({ msg: "updateCookies", url: t.meta.url.href });
-            });
-          });
-        });
-        delete c.headers["set-cookie"];
-      }
-      
       let finalBody;
-
       if (c.body) {
         const contentType = c.headers["content-type"] || "";
         const isHtmlContent = isHtml(t.meta.url, contentType);
@@ -143,56 +119,33 @@ class UVServiceWorker extends EventEmitter {
         
         if (needsRewrite) {
           const text = await a.text();
-          switch (e.destination) {
-            case "script":
-            case "worker":
-              // Force trust for Google scripts
-              finalBody = `if (!self.__uv && self.importScripts) importScripts('${__uv$config.bundle}', '${__uv$config.config}', '${__uv$config.handler}');\n${t.js.rewrite(text)}`;
-              break;
-            case "style":
-              finalBody = t.rewriteCSS(text);
-              break;
-            case "iframe":
-            case "document":
-              if (isHtmlContent) {
-                finalBody = t.rewriteHtml(text, {
-                  document: true,
-                  injectHead: t.createHtmlInject(
-                    this.config.handler,
-                    this.config.bundle,
-                    this.config.config,
-                    t.cookie.serialize(s, t.meta, true),
-                    e.referrer,
-                  ),
-                });
-                // Strip meta CSP tags from HTML
-                finalBody = finalBody.replace(/<meta[^>]*content-security-policy[^>]*>/gi, "");
-              } else {
-                finalBody = text;
-              }
-              break;
-            default:
-              if (isHtmlContent) {
-                 finalBody = t.rewriteHtml(text);
-              } else {
-                 finalBody = text;
-              }
+          if (isHtmlContent) {
+            finalBody = t.rewriteHtml(text, {
+              document: true,
+              injectHead: t.createHtmlInject(
+                this.config.handler,
+                this.config.bundle,
+                this.config.config,
+                t.cookie.serialize(s, t.meta, true),
+                e.referrer,
+              ),
+            });
+            // Wipe out any meta tags that mention CSP or security
+            finalBody = finalBody.replace(/<meta[^>]*content-security-policy[^>]*>/gi, "");
+            finalBody = finalBody.replace(/nonce="[^"]*"/gi, ""); // Remove nonces that block scripts
+          } else if (e.destination === "script" || e.destination === "worker") {
+            finalBody = t.js.rewrite(text);
+          } else if (e.destination === "style") {
+            finalBody = t.rewriteCSS(text);
+          } else {
+            finalBody = text;
           }
         } else {
           finalBody = c.body;
         }
-      } else {
-        finalBody = null;
       }
       
-      if (n.headers.accept === "text/event-stream") {
-        c.headers["content-type"] = "text/event-stream";
-      }
-      
-      this.emit("response", u);
-      if (u.intercepted) {
-        return u.returnValue;
-      }
+      this.emit("response", o);
       
       const finalStatus = (c.status < 200 || c.status > 599) ? 200 : c.status;
       
@@ -225,7 +178,6 @@ class UVServiceWorker extends EventEmitter {
   get address() {
     return this.addresses[Math.floor(Math.random() * this.addresses.length)];
   }
-  static Ultraviolet = Ultraviolet;
 }
 
 self.UVServiceWorker = UVServiceWorker;
@@ -250,15 +202,6 @@ class ResponseContext {
     this.status = data.status;
     this.statusText = data.statusText;
     this.body = data.body;
-  }
-  get url() {
-    return this.request.url;
-  }
-  get base() {
-    return this.request.base;
-  }
-  set base(e) {
-    this.request.base = e;
   }
 }
 
@@ -289,29 +232,20 @@ class RequestContext {
       "x-bare-headers": JSON.stringify(this.headers),
       "x-bare-forward-headers": JSON.stringify(this.forward),
     };
-    if (typeof userKey !== "undefined") {
-      bareHeaders.userKey = userKey;
-    }
     return new Request(url, {
       method: this.method,
       headers: bareHeaders,
       redirect: this.redirect,
       credentials: this.credentials,
-      mode: location.origin !== this.address.origin ? "cors" : this.mode,
+      mode: "cors",
       body: this.body,
     });
   }
   get url() {
     return this.ultraviolet.meta.url;
   }
-  set url(e) {
-    this.ultraviolet.meta.url = e;
-  }
   get base() {
     return this.ultraviolet.meta.base;
-  }
-  set base(e) {
-    this.ultraviolet.meta.base = e;
   }
 }
 
@@ -340,378 +274,16 @@ class HookEvent {
   }
 }
 
-let ReflectOwnKeys;
-const R = typeof Reflect === "object" ? Reflect : null;
-const ReflectApply =
-  R && typeof R.apply === "function"
-    ? R.apply
-    : (fn, thisArg, args) => Function.prototype.apply.call(fn, thisArg, args);
-
-function ProcessEmitWarning(warning) {
-  if (console && typeof console.warn === "function") {
-    console.warn(warning);
-  }
-}
-
-ReflectOwnKeys =
-  R && typeof R.ownKeys === "function"
-    ? R.ownKeys
-    : Object.getOwnPropertySymbols
-      ? (obj) => Object.getOwnPropertyNames(obj).concat(
-            Object.getOwnPropertySymbols(obj),
-          )
-      : (obj) => Object.getOwnPropertyNames(obj);
-
 function EventEmitter() {
-  EventEmitter.init.call(this);
+  this._events = Object.create(null);
 }
 
-EventEmitter.EventEmitter = EventEmitter;
-EventEmitter.prototype._events = undefined;
-EventEmitter.prototype._eventsCount = 0;
-EventEmitter.prototype._maxListeners = undefined;
-
-let defaultMaxListeners = 10;
-
-function checkListener(listener) {
-  if (typeof listener !== "function") {
-    throw new TypeError(
-      `The "listener" argument must be of type Function. Received type ${typeof listener}`,
-    );
-  }
-}
-
-function _getMaxListeners(emitter) {
-  if (emitter._maxListeners === undefined) {
-    return EventEmitter.defaultMaxListeners;
-  }
-  return emitter._maxListeners;
-}
-
-function _addListener(emitter, type, listener, prepend) {
-  let events;
-  let existing;
-  checkListener(listener);
-  events = emitter._events;
-  if (events === undefined) {
-    events = emitter._events = Object.create(null);
-    emitter._eventsCount = 0;
-  } else {
-    if (events.newListener !== undefined) {
-      emitter.emit("newListener", type, listener.listener ? listener.listener : listener);
-      events = emitter._events;
-    }
-    existing = events[type];
-  }
-  if (existing === undefined) {
-    existing = events[type] = listener;
-    ++emitter._eventsCount;
-  } else {
-    if (typeof existing === "function") {
-      existing = events[type] = prepend ? [listener, existing] : [existing, listener];
-    } else if (prepend) {
-      existing.unshift(listener);
-    } else {
-      existing.push(listener);
-    }
-    const m = _getMaxListeners(emitter);
-    if (m > 0 && existing.length > m && !existing.warned) {
-      existing.warned = true;
-      const err = new Error(
-        `Possible EventEmitter memory leak detected. ${existing.length} ${String(type)} listeners added. Use emitter.setMaxListeners() to increase limit`,
-      );
-      err.name = "MaxListenersExceededWarning";
-      err.emitter = emitter;
-      err.type = type;
-      err.count = existing.length;
-      ProcessEmitWarning(err);
-    }
-  }
-  return emitter;
-}
-
-function onceWrapper(...args) {
-  if (!this.fired) {
-    this.target.removeListener(this.type, this.wrapFn);
-    this.fired = true;
-    if (args.length === 0) {
-      return this.listener.call(this.target);
-    }
-    return this.listener.apply(this.target, args);
-  }
-}
-
-function _onceWrap(emitter, type, listener) {
-  const state = { fired: false, wrapFn: undefined, target: emitter, type, listener };
-  const wrapped = onceWrapper.bind(state);
-  wrapped.listener = listener;
-  state.wrapFn = wrapped;
-  return wrapped;
-}
-
-function _listeners(emitter, type, unwrap) {
-  const events = emitter._events;
-  if (events === undefined) {
-    return [];
-  }
-  const evlistener = events[type];
-  if (evlistener === undefined) {
-    return [];
-  }
-  if (typeof evlistener === "function") {
-    return unwrap ? [evlistener.listener || evlistener] : [evlistener];
-  }
-  return unwrap ? unwrapListeners(evlistener) : arrayClone(evlistener, evlistener.length);
-}
-
-function listenerCount(type) {
-  const events = this._events;
-  if (events !== undefined) {
-    const evlistener = events[type];
-    if (typeof evlistener === "function") {
-      return 1;
-    }
-    if (evlistener !== undefined) {
-      return evlistener.length;
-    }
-  }
-  return 0;
-}
-
-function arrayClone(arr, n) {
-  const copy = new Array(n);
-  for (let i = 0; i < n; ++i) {
-    copy[i] = arr[i];
-  }
-  return copy;
-}
-
-function spliceOne(list, index) {
-  for (let i = index; i + 1 < list.length; i++) {
-    list[i] = list[i + 1];
-  }
-  list.pop();
-}
-
-function unwrapListeners(arr) {
-  const ret = new Array(arr.length);
-  for (let i = 0; i < ret.length; ++i) {
-    ret[i] = arr[i].listener || arr[i];
-  }
-  return ret;
-}
-
-Object.defineProperty(EventEmitter, "defaultMaxListeners", {
-  enumerable: true,
-  get: () => defaultMaxListeners,
-  set: (arg) => {
-    if (typeof arg !== "number" || arg < 0 || Number.isNaN(arg)) {
-      throw new RangeError(
-        `The value of "defaultMaxListeners" is out of range. It must be a non-negative number. Received ${arg}.`,
-      );
-    }
-    defaultMaxListeners = arg;
-  },
-});
-
-EventEmitter.init = function () {
-  if (this._events === undefined || this._events === Object.getPrototypeOf(this)._events) {
-    this._events = Object.create(null);
-    this._eventsCount = 0;
-  }
-  this._maxListeners = this._maxListeners || undefined;
+EventEmitter.prototype.on = function(type, listener) {
+  if (!this._events[type]) this._events[type] = [];
+  this._events[type].push(listener);
 };
 
-EventEmitter.prototype.setMaxListeners = function (n) {
-  if (typeof n !== "number" || n < 0 || Number.isNaN(n)) {
-    throw new RangeError(
-      `The value of "n" is out of range. It must be a non-negative number. Received ${n}.`,
-    );
-  }
-  this._maxListeners = n;
-  return this;
-};
-
-EventEmitter.prototype.getMaxListeners = function () {
-  return _getMaxListeners(this);
-};
-
-EventEmitter.prototype.emit = function (type, ...args) {
-  let doError = type === "error";
-  const events = this._events;
-  if (events !== undefined) {
-    doError = doError && events.error === undefined;
-  } else if (!doError) {
-    return false;
-  }
-  if (doError) {
-    let er;
-    if (args.length > 0) {
-      er = args[0];
-    }
-    if (er instanceof Error) {
-      throw er;
-    }
-    const err = new Error(`Unhandled error.${er ? ` (${er.message})` : ""}`);
-    err.context = er;
-    throw err;
-  }
-  const handler = events[type];
-  if (handler === undefined) {
-    return false;
-  }
-  if (typeof handler === "function") {
-    ReflectApply(handler, this, args);
-  } else {
-    const len = handler.length;
-    const listeners = arrayClone(handler, len);
-    for (let j = 0; j < len; ++j) {
-      ReflectApply(listeners[j], this, args);
-    }
-  }
-  return true;
-};
-
-EventEmitter.prototype.addListener = function (type, listener) {
-  return _addListener(this, type, listener, false);
-};
-
-EventEmitter.prototype.on = EventEmitter.prototype.addListener;
-
-EventEmitter.prototype.prependListener = function (type, listener) {
-  return _addListener(this, type, listener, true);
-};
-
-EventEmitter.prototype.once = function (type, listener) {
-  checkListener(listener);
-  this.on(type, _onceWrap(this, type, listener));
-  return this;
-};
-
-EventEmitter.prototype.prependOnceListener = function (type, listener) {
-  checkListener(listener);
-  this.prependListener(type, _onceWrap(this, type, listener));
-  return this;
-};
-
-EventEmitter.prototype.removeListener = function (type, listener) {
-  let list;
-  let events;
-  let position;
-  let relistener;
-  checkListener(listener);
-  events = this._events;
-  if (events === undefined) {
-    return this;
-  }
-  list = events[type];
-  if (list === undefined) {
-    return this;
-  }
-  if (list === listener || list.listener === listener) {
-    if (--this._eventsCount === 0) {
-      this._events = Object.create(null);
-    } else {
-      delete events[type];
-      if (events.removeListener) {
-        this.emit("removeListener", type, list.listener || listener);
-      }
-    }
-  } else if (typeof list !== "function") {
-    position = -1;
-    for (let i = list.length - 1; i >= 0; i--) {
-      if (list[i] === listener || list[i].listener === listener) {
-        relistener = list[i].listener;
-        position = i;
-        break;
-      }
-    }
-    if (position < 0) {
-      return this;
-    }
-    if (position === 0) {
-      list.shift();
-    } else {
-      spliceOne(list, position);
-    }
-    if (list.length === 1) {
-      events[type] = list[0];
-    }
-    if (events.removeListener !== undefined) {
-      this.emit("removeListener", type, relistener || listener);
-    }
-  }
-  return this;
-};
-
-EventEmitter.prototype.off = EventEmitter.prototype.removeListener;
-
-EventEmitter.prototype.removeAllListeners = function (...args) {
-  let events;
-  let keys;
-  const type = args[0];
-  events = this._events;
-  if (events === undefined) {
-    return this;
-  }
-  if (events.removeListener === undefined) {
-    if (args.length === 0) {
-      this._events = Object.create(null);
-      this._eventsCount = 0;
-    } else if (events[type] !== undefined) {
-      if (--this._eventsCount === 0) {
-        this._events = Object.create(null);
-      } else {
-        delete events[type];
-      }
-    }
-    return this;
-  }
-  if (args.length === 0) {
-    keys = Object.keys(events);
-    for (let i = 0; i < keys.length; ++i) {
-      const key = keys[i];
-      if (key === "removeListener") {
-        continue;
-      }
-      this.removeAllListeners(key);
-    }
-    this.removeAllListeners("removeListener");
-    this._events = Object.create(null);
-    this._eventsCount = 0;
-    return this;
-  }
-  const handler = events[type];
-  if (typeof handler === "function") {
-    this.removeListener(type, handler);
-  } else if (handler !== undefined) {
-    for (let i = handler.length - 1; i >= 0; i--) {
-      this.removeListener(type, handler[i]);
-    }
-  }
-  return this;
-};
-
-EventEmitter.prototype.listeners = function (type) {
-  return _listeners(this, type, true);
-};
-
-EventEmitter.prototype.rawListeners = function (type) {
-  return _listeners(this, type, false);
-};
-
-EventEmitter.listenerCount = (emitter, type) => {
-  if (typeof emitter.listenerCount === "function") {
-    return emitter.listenerCount(type);
-  }
-  return listenerCount.call(emitter, type);
-};
-
-EventEmitter.prototype.listenerCount = listenerCount;
-
-EventEmitter.prototype.eventNames = function () {
-  if (this._eventsCount > 0) {
-    return ReflectOwnKeys(this._events);
-  }
-  return [];
+EventEmitter.prototype.emit = function(type, ...args) {
+  if (!this._events[type]) return;
+  this._events[type].forEach(listener => listener.apply(this, args));
 };
