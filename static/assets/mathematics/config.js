82e0dd0896e5f52726499694fcc46503a6e179cb
zakbooks <zak.books1@gmail.com>
2026-04-18 19:00:22 -0500

    Fix: Switch to tomp.app Bare server

--- static/assets/mathematics/config.js
+++ static/assets/mathematics/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/a/",
-  bare: "https://bare.benisland.xyz/",
+  bare: "https://bare.tomp.app/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/assets/mathematics/handler.js?v=9-30-2024",
