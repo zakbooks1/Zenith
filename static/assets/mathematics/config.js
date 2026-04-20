fceee48ec8aec1b49b91f7d3c9ca96f5b35e4bec
zakbooks1 <zak.books1@gmail.com>
2026-04-20 10:05:04 -0500

    Update config.js

--- static/assets/mathematics/config.js
+++ static/assets/mathematics/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/a/",
-  bare: "https://bare.benroxy.com/",
+  bare: "https://focus-bare-xi.vercel.app",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
 
